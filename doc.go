cfcdaed8b0281b4a454c2eedc12896b27eaf4163
Marcus <marcus@fractalqb.de>
2022-01-10 18:02:23 +0100

    Fix license text

--- doc.go
+++ doc.go
@@ -1,18 +1,18 @@
 // Copyright 2021 Marcus Perlick
 // This file is part of Go module github.com/fractalqb/change
 //
-// Foobar is free software: you can redistribute it and/or modify it
+// change is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// Foobar is distributed in the hope that it will be useful,
+// change is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+// along with change.  If not, see <http://www.gnu.org/licenses/>.
 
 // Package change has the common parts for the sub packages that help
 // to track changes of values in an application. The sub packages
